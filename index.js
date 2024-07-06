fa367c01ff64365d41125e83235c689b3e29148b
Muhib Anwar <160424220+MuhibAnwar@users.noreply.github.com>
2024-07-06 18:57:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Person {
+    personality;
+    constructor(personality = "Mystery") {
+        this.personality = personality;
+    }
+    userInput(input) {
+        if (input.includes('talk to other yourself in openly')) {
+            this.personality = 'Happy';
+        }
+        else if (input.includes('keep quite and dont ask')) {
+            this.personality = 'take rude';
+        }
+    }
+    get personality0() {
+        return this.personality;
+    }
+}
+class Main {
+    async main() {
+        const input = await inquirer.prompt({
+            name: "choices",
+            type: "list",
+            choices: ["talk to other yourself in openly", "keep quite and dont ask"],
+            message: chalk.yellow('TELL YOUR BEHAVIOUR')
+        });
+        let myperson = new Person();
+        myperson.userInput(input.choices);
+        console.log(`You are ${myperson.personality0}`);
+    }
+}
+const myobject = new Main();
+myobject.main();
